581ac2db0c617d999aba05b0ae0dd1921d2a959e
Douglas Molina <douglas.molina@intertecintl.com>
2016-11-18 00:25:44 -0600

    adding high order component for auth and signup flow

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER, AUTH_ERROR } from './types';
+import { 
+	AUTH_USER, 
+	AUTH_ERROR,
+	UNAUTH_USER 
+
+} from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -25,10 +30,36 @@ export function signinUser({ email, password }) {
 	};
 }
 
+export function signupUser({ email, password }) {
+	return function (dispatch) {
+		axios.post(`${ROOT_URL}/signup`, { email, password })
+			.then(response => {
+				//if request is good
+				// -update state if user authenticated
+				dispatch({ type: AUTH_USER });
+				// -save JWT token
+				localStorage.setItem('token', response.data.token);
+				// -redirect to the route '/feature'
+				browserHistory.push('/feature');	
+			})
+			.catch(error => {
+				dispatch(authError(error.response.data.error));
+			});		
+	};
+}
+
 export function authError(error) {
 	return {
 		type: AUTH_ERROR,
 		payload: error
+	};
+}
+
+export function signoutUser() {
+	localStorage.removeItem('token');
+	
+	return {
+		type: UNAUTH_USER,
 	};
 }
 
---
+++ client/src/components/feature.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Feature extends Component {
+
+	render() {
+		return (
+			<div>This is a feature to implement</div>
+		);
+	}
+}
+
+export default Feature;
